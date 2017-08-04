01eb3e40fe86a38efe14d875d1eb55435c4e89b6
Fernando Lozano <fernando@hdivsecurity.com>
2017-08-04 10:08:04 +0200

    Add REFLECTION_INJECTION vulnerability test

---
+++ src/main/java/com/vulnerable/StringCommand.java
@@ -0,0 +1,13 @@
+package com.vulnerable;
+
+public class StringCommand implements Runnable {
+
+	public StringCommand() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void run() {
+		System.out.println("Command executed!!");
+	}
+
+}
